91988e4eb7247a2ed40233b8b26bec1339340418
Nenad <nenad.milutinovicdev@gmail.com>
2019-04-16 14:57:09 +0200

    final code

---
+++ src/app/auth/auth-interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    const authToken = this.authService.getToken();
+
+    const authRequest = request.clone({
+      headers: request.headers.set('Authorization', 'Bearer ' + authToken)
+    });
+
+    return next.handle(authRequest);
+  }
+
+}
